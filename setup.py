9607014bfbe9fba46a65484801aa0e3e9514751e
dpressel <dpressel@gmail.com>
2022-04-27 22:27:24 -0400

    try based on pybind docs

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ def cpp_flag(compiler):
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
     c_opts = {
-        'msvc': ['/EHsc'],
+        'msvc': ['/EHsc', '/permissive-'],
         'unix': [],
     }
     l_opts = {
